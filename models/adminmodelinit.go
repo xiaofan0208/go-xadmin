d14f29300f7a468dba4c96fe247bdc57e5caf081
xiaofan0208 <xiaofan0208@126.com>
2020-06-25 23:23:44 +0800

    修改插入用户

--- models/adminmodelinit.go
+++ models/adminmodelinit.go
@@ -5,11 +5,13 @@ import "github.com/xiaofan0208/xbase/utils"
 // InsertAdminUser 插入用户
 func InsertAdminUser() {
 	user := &Backenduser{
-		Name:    "admin",
-		Email:   "admin@123.com",
-		IsAdmin: true,
+		Name:     "admin",
+		Email:    "admin@123.com",
+		Password: "123456",
+		IsAdmin:  true,
 	}
 	user.Salt = utils.RandomString(6)
+	user.Password = utils.EncodePassword(user.Password, user.Salt)
 
 	CreateBackenduser(user)
 }
